c969afd2f5eb4efb47cb749fc94fd08fca22b2e6
Fahim <fahim.madani@gmail.com>
2022-05-26 14:06:24 +0600

    verify jwt implemented

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aj1bp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorised access' })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded
+    next()
+  });
+
+
+}
+
+
+
+
+
+
 async function run() {
   try {
     await client.connect();
@@ -71,41 +93,69 @@ async function run() {
       const cursor = await reviewCollection.find({}).toArray()
       res.send(cursor)
     })
-    app.get("/order", async (req, res) => {
-      const cursor = await orderCollection.find({}).toArray()
-      res.send(cursor)
-    })
+    // app.get("/order", async (req, res) => {
+    //   const cursor = await orderCollection.find({}).toArray()
+    //   res.send(cursor)
+    // })
     app.post('/order', async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result)
     })
-    app.get('/order', async (req, res) => {
+    app.get('/order', verifyJWT, async (req, res) => {
       const email = req.query.email
-      const query = { email: email }
-      const cursor = orderCollection.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
+      const authorization = req.headers.authorization;
+      const decodedEmail = req.decoded.email
+      if (email === decodedEmail) {
+        const query = { email: email }
+        const cursor = orderCollection.find(query)
+        const result = await cursor.toArray();
+        return res.send(result)
+      }
+      else{
+        return res.status(403).send({message: "forbidden access"});
+      }
+
     })
     app.delete('/order/:id', async (req, res) => {
-      const {id} = req.params;
+      const { id } = req.params;
       const query = { _id: ObjectId(id) }
       console.log(id);
       const result = await orderCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.put('/user/:email', async(req, res) => {
-      const email = req.params.email; 
-      const user = req.body; 
-      const filter = {email: email}; 
-      const options = { upsert: true}; 
+    app.get('/user', async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users)
+    })
+
+    app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+
+      const filter = { email: email };
+      // const options = { upsert: true };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+
+      res.send(result);
+    })
+
+
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
       const updateDoc = {
         $set: user,
       };
-      const result = await usersCollection.updateOne(filter, updateDoc, options); 
-      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
-      res.send({result, token});
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
     })
 
 
